9cc36970c0c242848307831ece871516f97221fc
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-06-28 10:47:21 +0530

    feat: scaffold likes API and service layer

--- LinkUP/src/main/java/com/himanshu/LinkUP/controller/LikeController.java
+++ LinkUP/src/main/java/com/himanshu/LinkUP/controller/LikeController.java
@@ -2,8 +2,8 @@ package com.himanshu.LinkUP.controller;
 
 import com.himanshu.LinkUP.service.LikeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -11,6 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class LikeController {
     private final LikeService likeService;
 
+    @PostMapping("/{postId}/like")
+    public ResponseEntity<String> likeService(@PathVariable Long postId){
+        String message = likeService.likePost(postId);
+        return ResponseEntity.ok(message);
+    }
 
+    @PostMapping("/{postId}/unlike")
+    public ResponseEntity<String> unlikeService(@PathVariable Long postId){
+        String message = likeService.unlikePost(postId);
+        return ResponseEntity.ok(message);
+    }
+
+    @GetMapping("/{postId}/likes/count")
+    public ResponseEntity<Long> likeCount(@PathVariable Long postId){
+        Long count = likeService.likeCount(postId);
+        return ResponseEntity.ok(count);
+    }
 
 }
--- LinkUP/src/main/java/com/himanshu/LinkUP/service/impl/LikeServiceImpl.java
+++ LinkUP/src/main/java/com/himanshu/LinkUP/service/impl/LikeServiceImpl.java
@@ -1,11 +1,18 @@
 package com.himanshu.LinkUP.service.impl;
 
+import com.himanshu.LinkUP.repository.LikeRepository;
+import com.himanshu.LinkUP.repository.PostRepository;
+import com.himanshu.LinkUP.repository.UserRepository;
 import com.himanshu.LinkUP.service.LikeService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LikeServiceImpl implements LikeService {
-
+    private final LikeRepository likeRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
     @Override
     public String likePost(Long postId){
