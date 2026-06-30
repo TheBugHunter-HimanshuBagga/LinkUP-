461ee26da1e9b7b84c48716bfbb7eb09cb81892b
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-07-01 00:06:06 +0530

    feat: implement comments module

--- LinkUP/src/main/java/com/himanshu/LinkUP/service/impl/CommentServiceImpl.java
+++ LinkUP/src/main/java/com/himanshu/LinkUP/service/impl/CommentServiceImpl.java
@@ -2,30 +2,98 @@ package com.himanshu.LinkUP.service.impl;
 
 import com.himanshu.LinkUP.dto.CommentResponse;
 import com.himanshu.LinkUP.dto.CreateCommentRequest;
+import com.himanshu.LinkUP.entity.Comment;
+import com.himanshu.LinkUP.entity.Post;
+import com.himanshu.LinkUP.entity.User;
 import com.himanshu.LinkUP.repository.CommentRepository;
+import com.himanshu.LinkUP.repository.PostRepository;
+import com.himanshu.LinkUP.repository.UserRepository;
 import com.himanshu.LinkUP.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
-
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
     @Override
     public String addComment(Long postId, CreateCommentRequest request) {
-        return "";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        // CurrentUser should be existing to post the comment
+        User currentUser = userRepository.findByEmail(email)
+                .orElseThrow(
+                        () -> new RuntimeException("Current user not found")
+                );
+        // Post to be commented on should alos be existing
+        Post post = postRepository.findById(postId)
+                .orElseThrow(
+                        () -> new RuntimeException("Post Not Found on which you wanted to commebnt")
+                );
+        // if all perfect then
+        Comment comment = Comment.builder()
+                .content(request.getContent())
+                .user(currentUser)
+                .post(post)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        // save this post(comment) inside the CommentRepository
+        commentRepository.save(comment);
+        return "Comment posted successfully";
     }
 
     @Override
     public List<CommentResponse> getComments(Long postId) {
-        return List.of();
+
+        Post post = postRepository.findById(postId)
+                .orElseThrow(
+                        () -> new RuntimeException("Post not found")
+                );
+
+        return commentRepository.findByPost(post) // since from here i will be getting the list i will break it like [stream().map().toList()]
+                .stream()
+                .map(comment ->
+                        CommentResponse.builder()
+                                .id(comment.getId())
+                                .content(comment.getContent())
+                                .username(comment.getUser().getFullName())
+                                .createdAt(comment.getCreatedAt())
+                                .build()
+                )
+                .toList();
     }
 
     @Override
     public String deleteComment(Long commentId) {
-        return "";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        // CurrentUser should be existing to post the comment
+        User currentUser = userRepository.findByEmail(email)
+                .orElseThrow(
+                        () -> new RuntimeException("Current user not found")
+                );
+
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(
+                        ()-> new RuntimeException("comment not found")
+                );
+
+        if(!comment.getUser().getId().equals(currentUser.getId())){
+            throw new RuntimeException(
+                    "You are not authorized to delete this comment"
+            );
+        }
+        commentRepository.delete(comment);
+        return "Comment Deleted";
     }
 }
