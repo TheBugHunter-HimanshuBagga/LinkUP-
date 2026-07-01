1caae8f9a855d668b0ac83f17d56b9ce83e6f213
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-07-01 22:31:52 +0530

    feat: complete comments module with CRUD APIs

--- LinkUP/src/main/java/com/himanshu/LinkUP/controller/CommentController.java
+++ LinkUP/src/main/java/com/himanshu/LinkUP/controller/CommentController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/posts")
 @RequiredArgsConstructor
@@ -18,4 +20,18 @@ public class CommentController {
         String message = commentService.addComment(postId,request);
         return ResponseEntity.ok(message);
     }
+
+    @GetMapping("/{postId}/comments")
+    public ResponseEntity<List<CommentResponse>> getComments(@RequestBody Long postId){
+        List<CommentResponse> commentResponses = commentService.getComments(postId);
+        return ResponseEntity.ok(commentResponses);
+    }
+
+    @DeleteMapping("/comments/{commentId}")
+    public String deleteComment(@RequestBody Long commentId){
+        String message = commentService.deleteComment(commentId);
+        return message;
+    }
+
+
 }
